a2beb95f995524fa3ed2f97cc76fad1e9cf54f15
Ayush Jaiswal <ayush45aj@gmail.com>
2023-06-19 18:53:45 +0530

    second commit

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const formInput = require('../controllers/formInput.js')
-// const createMeet = require('../controllers/createMeet.js')
+const createMeet = require('../controllers/createMeet.js')
 const displayMeetings = require('../controllers/displayMeetings.js')
 
-router.post('/input', formInput)
-// router.post('/create-meet', createMeet)  //--Only this is getting error
+router.post('/create-meet', async (req, res) => {
+    res.render('../views/meet.html')
+})
 router.get('/display-meet', displayMeetings)
 
 module.exports = router;
